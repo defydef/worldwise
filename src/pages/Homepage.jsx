71ad68f904f3f331bf9eeb3b7e92f372581600c0
defy <8841170+defydef@users.noreply.github.com>
2023-08-30 08:07:45 +0700

    Add PageNav component for reusable page navigation

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
 
 function Homepage() {
   return (
     <div>
+      <PageNav />
       <h2>WorldWise</h2>
       <Link to="pricing">Pricing</Link>
     </div>
