beb553d0820c3be40429def867324fc02d726ecd
defy <8841170+defydef@users.noreply.github.com>
2023-09-01 08:31:02 +0700

    Add nested routes in App

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,11 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route path="cities" element={<p>List of cities</p>}></Route>
+          <Route path="countries" element={<p>List of countries</p>}></Route>
+          <Route path="form" element={<p>Form</p>}></Route>
+        </Route>
         <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
