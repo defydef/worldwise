1e358293bbecb53931a466813b6233fc71202d5b
defy <8841170+defydef@users.noreply.github.com>
2023-09-01 09:00:41 +0700

    Change height to 100vh in all pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
 
 function App() {
   return (
@@ -14,8 +15,8 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>LIST</p>} />
-          <Route path="cities" element={<p>List of cities</p>}></Route>
+          <Route index element={<CityList />} />
+          <Route path="cities" element={<CityList />}></Route>
           <Route path="countries" element={<p>List of countries</p>}></Route>
           <Route path="form" element={<p>Form</p>}></Route>
         </Route>
