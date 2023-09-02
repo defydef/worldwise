aee391a2e61c621d5a6746ac972fd1aaed73e8e2
defy <8841170+defydef@users.noreply.github.com>
2023-09-02 14:12:59 +0700

    Load cities using Fake API

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,38 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:8000";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    const controller = new AbortController();
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        if (!res.ok) throw new Error();
+        const data = await res.json();
+        if (data.Response === "False") throw new Error();
+        setCities(data);
+        console.log(data);
+      } catch {
+        alert("There was an error loading data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+    // cleanup function
+    return function () {
+      controller.abort();
+    };
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
