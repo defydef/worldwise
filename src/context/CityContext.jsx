a9734d33c17ca067f0fa507f4cb8a876a6e22a32
defy <8841170+defydef@users.noreply.github.com>
2023-09-07 07:59:54 +0700

    Get currentCity from CityContext

--- src/context/CityContext.jsx
+++ src/context/CityContext.jsx
@@ -8,6 +8,7 @@ const CityContext = createContext();
 function CityProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(function () {
     const controller = new AbortController();
@@ -20,7 +21,7 @@ function CityProvider({ children }) {
         if (data.Response === "False") throw new Error();
         setCities(data);
       } catch {
-        alert("There was an error loading data");
+        alert("There was an error loading list of cities data");
       } finally {
         setIsLoading(false);
       }
@@ -32,12 +33,29 @@ function CityProvider({ children }) {
     };
   }, []);
 
+  async function getCurrentCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      if (!res.ok) throw new Error();
+      const data = await res.json();
+      if (data.Response === "False") throw new Error();
+      setCurrentCity(data);
+    } catch {
+      alert("There was an error loading currentCity data");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   // 2. Provide value to child components
   return (
     <CityContext.Provider
       value={{
         cities,
         isLoading,
+        getCurrentCity,
+        currentCity,
       }}
     >
       {children}
