79972e9ed7b0884e7e5c552ae1e4371b6053bac6
defy <8841170+defydef@users.noreply.github.com>
2023-09-14 07:51:10 +0700

    Add comments on FakeAuthContext

--- src/context/FakeAuthContext.jsx
+++ src/context/FakeAuthContext.jsx
@@ -1,14 +1,19 @@
 import { createContext } from "react";
 
+// 1) Create a new Context component
 const AuthContext = createContext();
 
+// 2) Provide context values to children components
 function AuthProvider({ children }) {
   return <AuthContext.Provider>{children}</AuthContext.Provider>;
 }
 
 function useAuth() {
   const context = useContext(AuthContext);
-  if (context === undefined) throw new Error("useAuth is undefined");
+  if (context === undefined)
+    throw new Error(
+      "useAuth is undefined because it's used outside the AuthProvider component"
+    );
   return context;
 }
 
