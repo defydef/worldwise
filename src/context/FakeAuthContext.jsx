1af6669843520690ad8ea91b504bc71ddaa16b38
defy <8841170+defydef@users.noreply.github.com>
2023-09-15 08:31:42 +0700

    Add login and logout functionalities in FakeAuthContext

--- src/context/FakeAuthContext.jsx
+++ src/context/FakeAuthContext.jsx
@@ -1,11 +1,50 @@
-import { createContext } from "react";
+import { createContext, useContext, useReducer } from "react";
+
+const initialState = { user: null, isAuthenticated: false };
+
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return { ...state, user: action.payload, isAuthenticated: true };
+    case "logout":
+      return { ...state, user: null, isAuthenticated: false };
+    default:
+      throw new Error("Undefined action");
+  }
+}
 
 // 1) Create a new Context component
 const AuthContext = createContext();
 
 // 2) Provide context values to children components
 function AuthProvider({ children }) {
-  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password)
+      dispatch({ type: "login", payload: { email, password } });
+    else throw new Error("Incorrect email or password");
+  }
+
+  function logout() {
+    dispatch({ type: "logout" });
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 function useAuth() {
